56f7bef433b9a4e1e0171e2953831ddfc58d3d34
Steven Van de Poel <stevenvandepoel@gmail.com>
2021-02-25 20:28:14 +0100

    intersections

---
+++ intersections.js
@@ -0,0 +1,22 @@
+const get = (streets, cars) => {
+
+    const interSections = new Map()
+
+    const queue = new Map()
+
+    cars.forEach((car,index)=>{
+        queue[car.streets[0]] = [index]
+    })
+
+    streets.forEach(street => {
+        console.log(queue[street.name])
+        if(!interSections[street.endIntersec]) interSections[street.endIntersec] = []
+        interSections[street.endIntersec].push({name:street.name, state:false, queue:queue[street.name]||[]})
+    })
+
+    return interSections
+}
+
+module.exports = {
+    get
+}
